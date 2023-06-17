f62e38f7afb34d5767985f5cc969c173ccdbd3ec
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-16 19:30:07 -0600

    commit tree ee0bf19714221fe98d0424be9d34d1edba063d22

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,10 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] }
+    case "friends/remove":
+      return { friends: state.friends.filter(e => e.id !== action.payload) }
+    default:
+      return state
+  }
 }
